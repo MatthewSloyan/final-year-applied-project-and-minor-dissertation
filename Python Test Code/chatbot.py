5c251450009dd590103acefff0cac50b1163a0c8
Matthew Sloyan <g00348036@gmit.ie>
2019-10-20 14:08:05 +0100

    Re-engineered code to use Keras neural network instead of tflearn as it has been deprecated.

--- Python Test Code/chatbot.py
+++ Python Test Code/chatbot.py
@@ -1,16 +1,43 @@
-# Code adapted from the following tutorial
-# https://techwithtim.net/tutorials/ai-chatbot/part-1/
+# https://keras.io/getting-started/faq/#how-can-i-obtain-reproducible-results-using-keras-during-development
+# From our lecturer I found that it was best to seed the random weight generator to get reproducible results.
+# As TensorFlow will automatically assign random weights on each run based on probibility.
+# To achieve this I found documentation on the Keras website above, which I have adapted.
+import numpy as np
+import tensorflow as tf
+import random as rn
 
+# The below is necessary for starting Numpy and core Python generated random numbers in a well-defined initial state.
+np.random.seed(42)
+rn.seed(12345)
+
+# Force TensorFlow to use single thread.
+# Multiple threads are a potential source of non-reproducible results.
+# For further details, see: https://stackoverflow.com/questions/42022950/
+session_conf = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
+
+from keras import backend as K
+
+# The below tf.set_random_seed() will make random number generation
+# in the TensorFlow backend have a well-defined initial state.
+# For further details, see: https://www.tensorflow.org/api_docs/python/tf/set_random_seed
+tf.set_random_seed(1234)
+
+sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
+K.set_session(sess)
+
+# Additional Imports
+import keras as kr
+from keras.models import load_model # To save and load models
+import sklearn.preprocessing as pre # For encoding categorical variables.
+import json
+import pickle
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
-import numpy
-import tflearn
-import tensorflow
-import random
-import json
-import pickle
+# ==== IMPLEMENTATION =====
+# Code adapted from the following tutorial
+# https://techwithtim.net/tutorials/ai-chatbot/part-1/
 
 with open("Sources/intents.json") as file:
     data = json.load(file)
@@ -61,28 +88,25 @@ except:
         training.append(bag)
         output.append(output_row)
 
-
-    training = numpy.array(training)
-    output = numpy.array(output)
+    training = np.array(training)
+    output = np.array(output)
 
     with open("Sources/data.pickle", "wb") as f:
         pickle.dump((words, labels, training, output), f)
 
-tensorflow.reset_default_graph()
+# Set up a neural network model, building it layer by layer sequentially.
+model = kr.models.Sequential()
 
-net = tflearn.input_data(shape=[None, len(training[0])])
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
-net = tflearn.regression(net)
-
-model = tflearn.DNN(net)
+model.add(kr.layers.Dense(units=8, activation='linear', input_dim=len(training[0])))
+model.add(kr.layers.Dense(units=10, activation='relu')) 
+model.add(kr.layers.Dense(units=len(output[0]), activation='softmax'))
+model.compile(optimizer='rmsprop', loss='mse', metrics=['accuracy'])
 
 try:
-    model.load("Sources/model.tflearn")
+    model = load_model('Sources/test_model.h5')
 except:
-    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-    model.save("Sources/model.tflearn")
+    model.fit(training, output, epochs=100, batch_size=8)
+    model.save('Sources/test_model.h5')
 
 def bag_of_words(s, words):
     bag = [0 for _ in range(len(words))]
@@ -95,7 +119,7 @@ def bag_of_words(s, words):
             if w == se:
                 bag[i] = 1
             
-    return numpy.array(bag)
+    return np.array(bag)
 
 def chat():
     print("Start talking with the bot (type quit to stop)!")
@@ -104,14 +128,14 @@ def chat():
         if inp.lower() == "quit":
             break
 
-        results = model.predict([bag_of_words(inp, words)])
-        results_index = numpy.argmax(results)
+        results = model.predict([[bag_of_words(inp, words)]])
+        results_index = np.argmax(results)
         tag = labels[results_index]
 
         for tg in data["intents"]:
             if tg['tag'] == tag:
                 responses = tg['responses']
 
-        print(random.choice(responses))
+        print(rn.choice(responses))
 
 chat()
