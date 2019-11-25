fc1e6d68568737f853d64dc3f393ff681f47575f
aaronhannon <aaronchannon98@gmail.com>
2019-11-25 14:56:17 +0000

    FirstPersonController add

---
+++ UnityEngine/FinalYearUnity/Assets/Scripts/PersonCollider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonCollider : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("person"))
+        {
+            
+            Debug.Log("awake");
+        }
+
+
+        
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.CompareTag("person"))
+        {
+
+            Debug.Log("exit");
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
