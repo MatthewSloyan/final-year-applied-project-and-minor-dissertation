a94a85311dc82616625976b146e240dd790b5ea3
aaronhannon <aaronchannon98@gmail.com>
2020-01-07 22:06:35 +0000

    AIML Chatbot added

--- Chatbot/kerasChatbot.py
+++ Chatbot/kerasChatbot.py
@@ -77,5 +77,5 @@ def predictResponse():
     return response
 
 if __name__ == "__main__":
-    app.run(host = "192.168.1.7",debug = False, threaded = False)
+    app.run(debug = False, threaded = False)
 #host = "192.168.1.7",
