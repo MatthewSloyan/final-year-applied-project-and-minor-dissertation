45aba0d5b3baf9acc310b06af1e579f84b7d86b6
Matthew Sloyan <g00348036@gmit.ie>
2020-01-24 21:04:58 +0000

    Designed and added satifaction meter that moves on button press for now.

---
+++ UnityEngine/FinalYearUnity/Assets/__Scripts/SatisfactionMeter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SatisfactionMeter : MonoBehaviour
+{
+    private int satifaction;
+
+    void Start()
+    {
+        satifaction = 3;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            //gameObject.transform.localEulerAngles.z <= 76
+            if (satifaction > 1)
+            {
+                transform.Rotate(0f, 0f, 38);
+                satifaction--;
+                Debug.Log(satifaction);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (satifaction < 5)
+            {
+                transform.Rotate(0f, 0f, -38);
+                satifaction++;
+                Debug.Log(satifaction);
+            }
+        }
+    }
+}
