4191e304a53a090bfc437d1325216fdd5f093127
Matthew Sloyan <g00348036@gmit.ie>
2020-02-16 12:50:07 +0000

    Added tutorial menu, the user can now select and turn off the tutorial UI.

--- UnityEngine/FinalYearUnity/Assets/__Scripts/MenuController.cs
+++ UnityEngine/FinalYearUnity/Assets/__Scripts/MenuController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
@@ -48,7 +49,7 @@ public class MenuController : MonoBehaviour
     public void ResumeGame()
     {
         // Turn off the menu UI
-        pauseMenuUI.SetActive(false);
+        //pauseMenuUI.SetActive(false);
 
         // Start the game running again
         Time.timeScale = 1f;
@@ -58,10 +59,52 @@ public class MenuController : MonoBehaviour
     // Pauses game if called
     public void PauseGame()
     {
-        pauseMenuUI.SetActive(true);
+        //pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         isGamePaused = true;
     }
-    
+
+    // Display game over screen
+    public void GameOverDisplay()
+    {
+        //gameOverMenuUI.SetActive(true);
+    }
+
+    // Displays tutorial menu display.
+    public void DisplayTutorial(GameObject tutorialUI)
+    {
+        tutorialUI.SetActive(true);
+    }
+
+    // Changes tutorial page.
+    public void ChangeTutorial(GameObject tutorialUI)
+    {
+        tutorialUI.SetActive(true);
+    }
+
+    // Closes tutorial menu display.
+    public void CloseTutorial(GameObject tutorialUI)
+    {
+        tutorialUI.SetActive(false);
+    }
+    #endregion
+
+    #region == Navigation methods == 
+
+    // Resets game
+    public void ResetGame()
+    {
+        // Reload current scene abd un pause game.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    // Quit the game.
+    // Code adapted from: https://docs.unity3d.com/ScriptReference/Application.Quit.html
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
     #endregion
 }
