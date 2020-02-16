42efbd927bf0de232676466fd730a863e4d12328
Matthew Sloyan <g00348036@gmit.ie>
2020-02-16 12:31:13 +0000

    Added Menu controller script and basic methods to handle pause menu UI.

---
+++ UnityEngine/FinalYearUnity/Assets/__Scripts/MenuController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    #region == Private Variables == 
+    private static bool isGamePaused = false;
+
+    #endregion
+
+    // Singleton design pattern to get instance of class in PlayerCollider.cs
+    public static MenuController Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get esc key input from keyboard, to pause game from keyboard entry
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    #region == Menu overlays (Pause/Gameover)
+
+    // Resumes game if called
+    public void ResumeGame()
+    {
+        // Turn off the menu UI
+        pauseMenuUI.SetActive(false);
+
+        // Start the game running again
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    // Pauses game if called
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+    
+    #endregion
+}
