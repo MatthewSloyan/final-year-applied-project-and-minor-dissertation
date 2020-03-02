0d9b7b34ea2c07fba68a17f0d4a36521e80391c2
aaronhannon <aaronchannon98@gmail.com>
2020-03-02 19:46:27 +0000

    ScoreManager removed for now seems to be causing problems

--- UnityEngine/FinalYearUnityV2/Assets/_Scripts/Client.cs
+++ UnityEngine/FinalYearUnityV2/Assets/_Scripts/Client.cs
@@ -62,7 +62,9 @@ public class Client : MonoBehaviour
 
                 TextToSpeech.Instance.ConvertTextToSpeech(reponses[0], voiceName, false);
 
-                new ScoreManager(sessionId, Int32.Parse(reponses[1]), userInput, reponses[0]).UpdateScore();
+                //Could Be causing problems
+
+                //new ScoreManager(sessionId, Int32.Parse(reponses[1]), userInput, reponses[0]).UpdateScore();
             }
             catch
             { 
