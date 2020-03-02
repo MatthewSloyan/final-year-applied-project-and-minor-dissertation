2b8e670cccbec44a13ba9208f67b490ae8215aa4
aaronhannon <aaronchannon98@gmail.com>
2020-03-02 19:26:59 +0000

    Satisfaction meter code added to V2

--- UnityEngine/FinalYearUnityV2/Assets/_Scripts/PersonCollider.cs
+++ UnityEngine/FinalYearUnityV2/Assets/_Scripts/PersonCollider.cs
@@ -15,26 +15,26 @@ public class PersonCollider : MonoBehaviour
         //    // Get the NPC script attached to NPC and start to listen for speech.
             NPC npc = col.gameObject.GetComponent<NPC>();
             Debug.Log("Collision");
-            //    // Change ring colour to blue to to signify that the player can talk to the NPC.
-            //    CompleteRing cr = col.gameObject.GetComponentInChildren<CompleteRing>();
+            // Change ring colour to blue to to signify that the player can talk to the NPC.
+            CompleteRing cr = col.gameObject.GetComponentInChildren<CompleteRing>();
 
-            //    // Find satisfaction meter gameobject and set to active so it's displayed.
-            //    GameObject temp = col.gameObject.transform.Find("SatisfactionMeterContainer(Clone)").gameObject;
-            //    temp.SetActive(true);
+            // Find satisfaction meter gameobject and set to active so it's displayed.
+            GameObject temp = col.gameObject.transform.Find("SatisfactionMeterContainer(Clone)").gameObject;
+            temp.SetActive(true);
 
-            //    SpeechToText.IsPersonActive = true;
+            SpeechToText.IsPersonActive = true;
 
             //    // Check if ticket has been checked or not.
-            //    if (cr.getCurrentColour() == "Green")
-            //    {
-            //        TextToSpeech.Instance.ConvertTextToSpeech(replies[npc.GetPersona()], npc.GetVoiceName(), true);
-            //    }
-            //    else
-            //    {
-            //        cr.changeRingColour("Blue");
+            if (cr.getCurrentColour() == "Green")
+            {
+                TextToSpeech.Instance.ConvertTextToSpeech(replies[npc.GetPersona()], npc.GetVoiceName(), true);
+            }
+            else
+            {
+                cr.changeRingColour("Blue");
 
-            //        SpeechToText.Instance.convertSpeechToText(npc.GetSessionID(), npc.GetPersona(), npc.GetVoiceName());
-            //    }
+                SpeechToText.Instance.convertSpeechToText(npc.GetSessionID(), npc.GetPersona(), npc.GetVoiceName());
+            }
             SpeechToText.IsPersonActive = true;
             SpeechToText.Instance.convertSpeechToText(npc.GetSessionID(), npc.GetPersona(), npc.GetVoiceName());
         }
@@ -45,25 +45,25 @@ public class PersonCollider : MonoBehaviour
     {
         if (col.gameObject.CompareTag("person"))
         {
-            //var renderer = col.gameObject.GetComponent<Renderer>();
-            //renderer.material.SetColor("_Color", Color.white);
+            var renderer = col.gameObject.GetComponent<Renderer>();
+            renderer.material.SetColor("_Color", Color.white);
 
-            // Check if colour is green.
-            //string colour = col.gameObject.GetComponentInChildren<CompleteRing>().getCurrentColour();
-            ////Debug.Log(colour);
-            ////Debug.Log(tempColour);
+            //Check if colour is green.
+            string colour = col.gameObject.GetComponentInChildren<CompleteRing>().getCurrentColour();
+            //Debug.Log(colour);
+            //Debug.Log(tempColour);
 
-            //if (colour != "Green")
-            //{
-            //    // Change ring colour back to initial colour when player walks away from NPC.
-            //    col.gameObject.GetComponentInChildren<CompleteRing>().changeRingColour("Red");
-            //}
+            if (colour != "Green")
+            {
+                // Change ring colour back to initial colour when player walks away from NPC.
+                col.gameObject.GetComponentInChildren<CompleteRing>().changeRingColour("Red");
+            }
 
             SpeechToText.IsPersonActive = false;
 
             // Find satisfaction meter gameobject and set to not active so it's hidden.
-            //GameObject temp = col.gameObject.transform.Find("SatisfactionMeterContainer(Clone)").gameObject;
-            //temp.SetActive(false);
+            GameObject temp = col.gameObject.transform.Find("SatisfactionMeterContainer(Clone)").gameObject;
+            temp.SetActive(false);
         }
     }
 }
