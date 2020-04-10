f19daebe07b36e1cb3aba48fe139b3a162df82fd
Matthew Sloyan <g00348036@gmit.ie>
2020-04-10 14:25:33 +0100

    Added more Unity testing using NUnit.

---
+++ UnityEngine/FinalYearUnity/Assets/Tests/TestSuite.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestSuite
+    {
+        //private ScoreManager sm;
+
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TestScoreManager()
+        {
+            //sm = new ScoreManager(1234, 1, "Hello", "Hello, pleased to meet you!").UpdateScore();
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator TestSuiteWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
