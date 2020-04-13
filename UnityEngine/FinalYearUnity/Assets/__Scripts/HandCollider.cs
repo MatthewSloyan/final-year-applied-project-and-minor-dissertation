3d021885939134c922fbbd4d426af8e5c29bf2bb
Matthew Sloyan <g00348036@gmit.ie>
2020-04-13 16:32:04 +0100

    Moved pause menu functionality onto to wristwatch for easier access.

--- UnityEngine/FinalYearUnity/Assets/__Scripts/HandCollider.cs
+++ UnityEngine/FinalYearUnity/Assets/__Scripts/HandCollider.cs
@@ -4,19 +4,58 @@ using UnityEngine;
 
 public class HandCollider : MonoBehaviour
 {
+    #region == Variables == 
+    private static bool isGamePaused = false;
+
+    public static bool IsGamePaused
+    {
+        get { return isGamePaused; }
+        set { isGamePaused = value; }
+    }
+
+    private GameObject pauseMenu;
+    #endregion
+
+    void Start()
+    {
+        pauseMenu = GameObject.Find("PauseMenuWatch");
+        pauseMenu.GetComponent<Canvas>().enabled = false;
+        //pauseMenu.enabled = false;
+    }
+
     void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.CompareTag("watch"))
         {
-            
+            if (isGamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
         }
     }
 
-    private void OnTriggerExit(Collider col)
+    // Resumes game if called
+    public void ResumeGame()
     {
-        if (col.gameObject.CompareTag("watch"))
-        {
-           
-        }
+        Debug.Log("Resume");
+        // Turn off the menu UI
+        pauseMenu.GetComponent<Canvas>().enabled = false;
+
+        // Start the game running again
+        //Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    // Pauses game if called
+    public void PauseGame()
+    {
+        pauseMenu.GetComponent<Canvas>().enabled = true;
+
+        //Time.timeScale = 0f;
+        isGamePaused = true;
     }
 }
