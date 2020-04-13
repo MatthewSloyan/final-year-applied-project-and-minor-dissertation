0f40c618350278e24d145419cbab2c0a8c170184
Matthew Sloyan <g00348036@gmit.ie>
2020-04-13 14:30:12 +0100

    Added virtual hands and real time watch.

---
+++ UnityEngine/FinalYearUnity/Assets/__Scripts/HandCollider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandCollider : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("watch"))
+        {
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.CompareTag("watch"))
+        {
+           
+        }
+    }
+}
