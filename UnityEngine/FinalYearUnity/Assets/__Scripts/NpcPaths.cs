3cea8f69809d413b438e2a26adc996d1e01a0556
aaronhannon <aaronchannon98@gmail.com>
2020-04-14 14:35:20 +0100

    Multiple Npcs follow the path

--- UnityEngine/FinalYearUnity/Assets/__Scripts/NpcPaths.cs
+++ UnityEngine/FinalYearUnity/Assets/__Scripts/NpcPaths.cs
@@ -6,26 +6,40 @@ public class NpcPaths : MonoBehaviour
 {
 
     public Transform[] target;
+    public Transform[] target1;
     public float speed;
 
-    public int current;
-
+    private int current;
+    private int lastNode;
     void Start()
     {
-        
+        current = 1;
+        GameObject container = GameObject.Find("NPC1targets");
+        target1 = container.GetComponentsInChildren<Transform>();
+
+        foreach (var item in target1)
+        {
+            Debug.Log(item.name);
+        }
+
+        lastNode = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position != target[current].position){
+        if(lastNode < target1.Length-1){
+            if(transform.position != target1[current].position ){
 
-            Vector3 pos = Vector3.MoveTowards(transform.position,target[current].position,speed * Time.deltaTime);
+                Vector3 pos = Vector3.MoveTowards(transform.position,target1[current].position,speed * Time.deltaTime);
 
-            GetComponent<Rigidbody>().MovePosition(pos);
+                GetComponent<Rigidbody>().MovePosition(pos);
 
-        }else{
-            current = (current+1) % target.Length;
+            }else{
+                current = (current+1) % target1.Length;
+                lastNode++;
+                Debug.Log("Node" + lastNode);
+            }
         }
     }
 }
