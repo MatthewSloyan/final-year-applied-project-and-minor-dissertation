b0a133f1f64f3f9a8c8bbe3575d72ca2a8a6b8ee
Matthew Sloyan <g00348036@gmit.ie>
2020-04-17 14:32:43 +0100

    Added script to start video playing.

---
+++ UnityEngine/FinalYearUnity/Assets/__Scripts/PlayVideo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class PlayVideo : MonoBehaviour
+{
+    public RawImage rawImage;
+    public VideoPlayer videoPlayer;
+    public AudioSource audioSource;
+    
+    // Use this for initialization
+    void Start () {
+        // Start playing the video.
+        StartCoroutine(StartVideo());
+    }
+
+    // Code adapted from: https://www.mirimad.com/unity-play-video-on-canvas/
+    IEnumerator StartVideo(){
+        // Prepare video and wait till ready.
+        videoPlayer.Prepare();
+
+        while (!videoPlayer.isPrepared)
+        {
+            yield return new WaitForSeconds(1);
+        }
+
+        // Once video is ready, load into canvas and play video and audio.
+        rawImage.texture = videoPlayer.texture;
+        videoPlayer.Play();
+        audioSource.Play();
+    }
+}
